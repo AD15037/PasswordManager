5aca4c4a1bcfd1f4add86b80e82b43b550bd019e
AD15037 <atharvdwivedi15037@gmail.com>
2025-12-26 17:06:40 +0530

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Atharv Dwivedi on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
